a2d9730d6b7006c02b9c9fc63f57d1dcbfe1cabc
Onur Alp <onuralpkayag@gmail.com>
2020-11-29 17:54:24 +0300

    JTP1, lat2eqdist, dist2eqlat, longt2dist, ellipsoidArea, radiusCurveture methods.

--- Ellipsoid.m
+++ Ellipsoid.m
@@ -78,6 +78,7 @@ classdef Ellipsoid < NivoEllipsoid
     end
     
     methods
+        
         %   args:
         %
         %
@@ -168,7 +169,7 @@ classdef Ellipsoid < NivoEllipsoid
         %   returns:
         %
         %
-        function [A1, A2, S] = JTP2(ellipsoid, B1, L1, B2, L2)
+        function [A1, A2, S] = JTP2(ellipsoid, B1, B2, L1, L2)
             b_ = ellipsoid.SemiminorAxis;
             f = ellipsoid.Flattening;
             ec = ellipsoid.SecondEccentricity;
@@ -184,7 +185,8 @@ classdef Ellipsoid < NivoEllipsoid
             dL_old = inf;
             while abs(dL - dL_old) > 1e-12
                 dL_old = dL;
-                snq = (sqrt((cos(u_) * sin(dL))^2 + (cos(u) * sin(u_) - sin(u) * cos(u_) * cos(dL))^2));
+                snq = (sqrt((cos(u_) * sin(dL))^2 + ...
+                    (cos(u) * sin(u_) - sin(u) * cos(u_) * cos(dL))^2));
                 csq = sin(u) * sin(u_) + cos(u) * cos(u_) * cos(dL);
                 
                 q = atan2(snq, csq);
@@ -198,7 +200,8 @@ classdef Ellipsoid < NivoEllipsoid
             u2_ = ec2 * cos(Al)^2;
             a__ = 1 + (u2_ / 16384) * (4096 + u2_ * (-768 + u2_ * (320 - 175 * u2_)));
             b__ = (u2_ / 1024) * (256 + u2_ * (-128 + u2_ * (74 - 47*u2_)));
-            dq = b__ * snq * (km + (b__ / 4) * (csq * (-1 + 2 * km^2) - (b__ / 6) * km * (-3 + 4 * snq^2) * (-3 + 4 * km^2)));
+            dq = b__ * snq * (km + (b__ / 4) * (csq * (-1 + 2 * km^2) - ...
+                (b__ / 6) * km * (-3 + 4 * snq^2) * (-3 + 4 * km^2)));
             
             S = b_ * a__ *abs(q - dq);
             A1 = atan2(cos(u_) * sin(dL), (cos(u) * sin(u_) - sin(u) * cos(u_) * cos(dL)));
@@ -209,6 +212,208 @@ classdef Ellipsoid < NivoEllipsoid
                 A2 = rad2deg(A2 - pi);
             end
             A1 = rad2deg(A1);
+        end
+        
+        %   args:
+        %
+        %
+        %   returns:
+        %
+        %
+        function [B2, L2, A2] = JTP1(ellipsoid, B1, L1, A1, S)
+            b_ = ellipsoid.SemiminorAxis;
+            f = ellipsoid.Flattening;
+            ec = ellipsoid.SecondEccentricity;
+            ec2 = ec * ec;
+            
+            B1 = deg2rad(B1); L1 = deg2rad(L1); A1 = deg2rad(A1);
+            
+            u_ = atan((1 - f) * tan(B1));
+            p_ = atan(tan(u_) / cos(A1));
+            alpha = asin(cos(u_) * sin(A1));
+            u2_ = cos(alpha) * cos(alpha) * ec2;
+            
+            a__ = 1 + (u2_ / 16384) * (4096 + u2_ * (-768 + u2_ * (320 - 175 * u2_)));
+            b__ = (u2_ / 1024) * (256 + u2_ * (-128 + u2_ * (74 - 47 * u2_)));
+            
+            p = S / (b_ * a__);
+            
+            p_old = inf;
+            while abs(p - p_old) > 1e-13
+                p_old = p;
+                qm2 = 2 * p_ + p;
+                km = cos(qm2);
+                dq = b__ * sin(p) * (km + (b__ / 4) * (cos(p) * (-1 + 2 * km * km) - ...
+                    (b__ / 6) * km * (-3 + 4 * sin(p) * sin(p)) * (-3 + 4 * km * km)));
+                p = S / (b_ * a__) + dq;
+            end
+            
+            B2 = atan((sin(u_) * cos(p) + cos(u_) * sin(p) * cos(A1)) / ...
+                ((1 - f) * (sin(alpha) * sin(alpha) + (sin(u_) * sin(p) - cos(u_) * cos(p) * cos(A1))^2) ^ (1 / 2)));
+            L_ = atan((sin(p) * sin(A1)) / (cos(u_) * cos(p) - sin(u_) * sin(p) * cos(A1)));
+            km2 = 2 * p_ + p;
+            c = (f / 16) * cos(alpha) * cos(alpha) * (4 + f * (4 - 3 * cos(alpha) * cos(alpha)));
+            dL = L_ - (1 - c) * f * sin(alpha) * ...
+                (p + c * sin(p) * (cos(km2) + c * cos(p) * (-1 + 2 * cos(km2) * cos(km2))));
+            L2 = L1 + dL;
+            A2 = atan2(sin(alpha), (-sin(u_) * sin(p) + cos(u_) * cos(p) * cos(A1)));
+            if A1 < pi
+                A2 = rad2deg(A2 + pi);
+            else
+                A2 = rad2deg(A2 - pi);
+            end
+            B2 = rad2deg(B2);
+            L2 = rad2deg(L2);
+        end
+        
+        %   args:
+        %
+        %
+        %   returns:
+        %
+        %
+        function G = lat2eqdist(ellipsoid, B)
+            a_  = ellipsoid.SemimajorAxis ;
+            b_  = ellipsoid.SemiminorAxis ;
+            c  = a_^2 / b_ ;
+            eu2 = ellipsoid.SecondEccentricity^2 ;
+            invro = pi / 180 ;
+
+            sinf = @sind ;
+
+            A_ = c * (1 - 3/4*eu2 + 45/64*eu2^2 - 175/256*eu2^3 + 11025/16384*eu2^4 - 43659/65536*eu2^5) * invro;
+            B_ = c * (-3/8*eu2 + 15/32*eu2^2 - 525/1024*eu2^3 + 2205/4096*eu2^4 - 72765/131072*eu2^5) ;
+            C_ = c * (15/256*eu2^2 - 105/1024*eu2^3 + 2205/16384*eu2^4 - 10395/65536*eu2^5) ;
+            D_ = c * (-35/3072*eu2^3 + 315/12288*eu2^4 - 31185/786432*eu2^5) ;
+            E_ = c * (315/131072*eu2^4 - 3465/524288*eu2^5 ) ;
+            F_ = c * (-693/1310720*eu2^5) ;
+
+            G = A_*B + B_*sinf(2*B) + C_*sinf(4*B) + D_*sinf(6*B) + E_*sinf(8*B) + F_*sinf(10*B);
+        end
+        
+        %   args:
+        %
+        %
+        %   returns:
+        %
+        %
+        function B = dist2eqlat(ellipsoid, G)
+            a_  = ellipsoid.SemimajorAxis ;
+            b_  = ellipsoid.SemiminorAxis ;
+            c  = a_^2 / b_ ;
+            eu2 = ellipsoid.SecondEccentricity^2 ;
+            ro = 180 / pi ;
+
+            sinf = @sind ;
+
+            A_ = c * (1 - 3/4*eu2 + 45/64*eu2^2 - 175/256*eu2^3 + 11025/16384*eu2^4 - 43659/65536*eu2^5) / ro; %... * invro
+            B__= (3/8*eu2 - 3/16*eu2^2 + 213/2048*eu2^3 - 255/4096*eu2^4 + 20861/524288*eu2^5) * ro ;
+            C__= (21/256*eu2^2 - 21/256*eu2^3 + 533/8192*eu2^4 - 197/4096*eu2^5) * ro ;
+            D__= (151/6144*eu2^3 - 453/12288*eu2^4 + 5019/131072*eu2^5) * ro ;
+            E__= (1097/131072*eu2^4 - 1097/65536*eu2^5) * ro ;
+            F__= (8011/2621440*eu2^5) * ro ;
+
+            sigma = G / A_ ;
+
+            B = sigma + B__*sinf(2*sigma) + C__*sinf(4*sigma) + D__*sinf(6*sigma) + ...
+                E__*sinf(8*sigma) + F__*sinf(10*sigma) ;
+        end
+        
+        %   args:
+        %
+        %
+        %   returns:
+        %
+        %
+        function Sp = longt2dist(ellipsoid, L1, L2, B)
+            a_  = ellipsoid.SemimajorAxis ;
+            b_  = ellipsoid.SemiminorAxis ;
+            c   = a_^2 / b_ ;
+            e2  = ellipsoid.Eccentricity^2 ;
+            eu2 = ellipsoid.SecondEccentricity^2 ;
+            
+            sinf = @sind ;
+            cosf = @cosd ;
+            
+            n2 = eu2 * cosf(B).^2 ;
+            V = sqrt(1 + n2) ;
+            W = sqrt(1 - e2*sinf(B).^2) ;
+            
+            N_ = c ./ V ;
+            N__= a_ ./ W ;
+            N = (N_ + N__) ./ 2 ;
+            
+            l = L2 - L1 ;
+            
+            Sp = N * l * cosf(B) * pi / 180; % pi / 180 -> radians
+        end
+
+        %   args:
+        %
+        %
+        %   returns:
+        %
+        %
+        function F = ellipsoidArea(ellipsoid, B1, B2, L1, L2)
+
+            b_  = ellipsoid.SemiminorAxis ;
+            e2  = ellipsoid.Eccentricity^2 ;
+            
+            Fa_ = 1 + 1/2*e2 + 3/8*e2^2 + 5/16*e2^3 + 35/128*e2^4 ;
+            Fb_ = 1/6*e2 + 3/16*e2^2 + 3/16*e2^3 + 35/192*e2^4 ;
+            Fc_ = 3/80*e2^2 + 1/16*e2^3 + 5/64*e2^4 ;
+            Fd_ = 1/112*e2^3 + 5/256*e2^4 ;
+            Fe_ = 5/2304*e2^4 ;
+            
+            dB = B2 - B1 ;
+            dL = L2 - L1 ;
+            Bm = (B1 + B2) / 2 ; % mean B
+            
+            sinf = @sind ;
+            cosf = @cosd ;
+            
+            % Z = 2*pi*b^2 INTEGRAL(B1 -> B2) cos(B) / (1 - e^2*sin^2*B)^2
+            % --> dB
+            Z_ = 4 * pi * b_^2 * ( ...
+                Fa_*sinf(dB/2)*cosf(Bm) - ...
+                Fb_*sinf(3*dB/2)*cosf(3*Bm) + ...
+                Fc_*sinf(5*dB/2)*cosf(5*Bm) - ...
+                Fd_*sinf(7*dB/2)*cosf(7*Bm) + ...
+                Fe_*sinf(9*dB/2)*cosf(9*Bm) ) ; % m^2
+            
+            Z = Z_ / 1e6 ; % km^2
+            F = dL * Z / (2*pi) * pi / 180; % km^2
+        end
+        
+        %   args:
+        %
+        %
+        %   returns:
+        %
+        %
+        function [N, M, R, varargout] = radiusCurveture(ellipsoid, B, A)
+            a_ = ellipsoid.SemimajorAxis;
+            eu = ellipsoid.SecondEccentricity; eu2 = eu * eu;
+            invf_ = ellipsoid.InverseFlattening;
+            c = a_ / (1 - 1 / invf_); 
+            
+            B = deg2rad(B);
+
+            n2 = eu2 * cos(B)^2;
+            V = sqrt(1 + n2);
+ 
+            N = c / V;
+            M = c / V^3;
+            R = c / V^2;
+   
+            if nargin > 2
+                A = deg2rad(A);
+                Ra = M*N / (M*sin(A)^2 + N * cos(A)^2);
+                varargout{1} = Ra;
+            end
+            if M > R || R > N || M > N
+                warning('incorrect radius of curvature')
+            end
         end
     end
 end
--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clc;clearvars;
-ellipsoid = ReferenceEllipsoid('hayford');
+ellipsoid = ReferenceEllipsoid('grs80');
 
- 
+%%
 % geocentric
 % B = 41.27737178;
 % L = 39.21259856;
@@ -23,9 +23,51 @@ ellipsoid = ReferenceEllipsoid('hayford');
 %%
 
 % JTP2:
-B1 = 40.19043611;
-L1 = 33.20710278;
-B2 = 41.74048611;
-L2 = 33.72431111;
+% B1 = 40.19043611;
+% L1 = 33.20710278;
+% B2 = 41.74048611;
+% L2 = 33.72431111;
+% 
+% [A1, A2, S] = JTP2(ellipsoid, B1, B2, L1, L2);
+%%
 
-[A1, A2, S] = JTP2(ellipsoid, B1, L1, B2, L2);
+% JTP1:
+% B1 = 39.50765833;
+% L1 = 39.35715278;
+% A1 = 141.6904861;
+% S = 69876.5752;
+% 
+% [B2, L2, A2] = JTP1(ellipsoid, B1, L1, A1, S);
+
+%%
+% G to B;
+% G = 4314212.496;
+% B = dist2eqlat(ellipsoid, G);
+
+%%
+% B to G;
+% B = 38.0000;
+% G = lat2eqdist(ellipsoid, B);
+
+%%
+% L to Sp
+% L1 = 26;
+% L2 = 45;
+% B = 36;
+% 
+% Sp = longt2dist(ellipsoid, L1, L2, B);
+
+%% 
+% Area
+% B1 = 39; B2 = 40;
+% L1 = 28; L2 = 29;
+% 
+% F = ellipsoidArea(ellipsoid, B1, B2, L1, L2);
+
+%%
+% radius of curvature
+% B = 41.27737178;
+% A = 42.33333333;
+
+% [M, N, R] = radiusCurveture(ellipsoid, B);
+% [M, N, R, Ra] = radiusCurveture(ellipsoid, B, A);
